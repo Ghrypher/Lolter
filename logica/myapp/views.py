c6bedfd17bfced8f20471e9c0cc4c0e22f24c804
ghrypher <codicegmaer@gmail.com>
2022-11-17 23:51:59 -0300

    Los filtros son funcionales con querys y se suman entre ellos

--- logica/myapp/views.py
+++ logica/myapp/views.py
@@ -8,7 +8,7 @@ from django.db import connection
 #principal, y la pagina de counter, a la cual se accederia una vez clickeado en un champ, mostrando
 #toda la data sobre los counters de ese champ
 
-def obtenerChamps(champ):
+def obtenerChamps(champ, rol = None, busqueda = None):
     #champ ejemplo
         #champ[
         #   id (no actualmente)
@@ -16,7 +16,46 @@ def obtenerChamps(champ):
         #   [rol1, rol2, rol3]
         #   [[counter1, descripcion1], [counter2, descripcion2], [counter3, descripcion3]]
         #]   
-    if champ == "All":
+    rows = []
+    if champ == "Filtrar":
+        if  rol:
+            if busqueda:
+                with connection.cursor() as cursor:
+                    cursor.execute("""  SELECT champ.id, champ.nombre, roles.Rol, counter.counters, counter.descrpipciones FROM myapp_champ as champ
+                                        JOIN (SELECT champrol.Champ_id, group_concat(roles.Rol, '@') as Rol FROM myapp_champrol as champrol
+                                                    JOIN myapp_roles as roles ON champrol.Rol_id = roles.id
+                                                    GROUP BY champrol.Champ_id) as roles ON roles.Champ_id = champ.id
+                                        JOIN (SELECT  counter.champ_selected_id, group_concat(champs.nombre , '@') as counters, group_concat(counter.description , '@') as descrpipciones  FROM myapp_counter as counter
+                                                    JOIN myapp_champ as champs ON counter.champ_counter_id = champs.id
+                                                    GROUP BY counter.champ_selected_id	) as counter ON counter.champ_selected_id = champ.id
+                                    WHERE champ.nombre LIKE '""" + busqueda + "%' AND roles.Rol LIKE '%" + rol + "%'")
+                    rows = cursor.fetchall()
+                
+            else:
+                with connection.cursor() as cursor:
+                    cursor.execute("""  SELECT champ.id, champ.nombre, roles.Rol, counter.counters FROM myapp_champ as champ
+                                                JOIN (SELECT champrol.Champ_id, group_concat(roles.Rol, '@') as Rol FROM myapp_champrol as champrol
+                                                            JOIN myapp_roles as roles ON champrol.Rol_id = roles.id
+                                                            GROUP BY champrol.Champ_id) as roles ON roles.Champ_id = champ.id
+                                                JOIN (SELECT  counter.champ_selected_id, group_concat(champs.nombre , '@') as counters FROM myapp_counter as counter
+                                                            JOIN myapp_champ as champs ON counter.champ_counter_id = champs.id
+                                                            GROUP BY counter.champ_selected_id	) as counter ON counter.champ_selected_id = champ.id
+                                            WHERE roles.Rol LIKE '%""" + rol + "%'")
+                    rows = cursor.fetchall()
+
+        elif busqueda:
+            with connection.cursor() as cursor:
+                cursor.execute("""  SELECT champ.id, champ.nombre, roles.Rol, counter.counters, counter.descrpipciones FROM myapp_champ as champ
+                                        JOIN (SELECT champrol.Champ_id, group_concat(roles.Rol, '@') as Rol FROM myapp_champrol as champrol
+                                                    JOIN myapp_roles as roles ON champrol.Rol_id = roles.id
+                                                    GROUP BY champrol.Champ_id) as roles ON roles.Champ_id = champ.id
+                                        JOIN (SELECT  counter.champ_selected_id, group_concat(champs.nombre , '@') as counters, group_concat(counter.description , '@') as descrpipciones  FROM myapp_counter as counter
+                                                    JOIN myapp_champ as champs ON counter.champ_counter_id = champs.id
+                                                    GROUP BY counter.champ_selected_id	) as counter ON counter.champ_selected_id = champ.id
+                                    WHERE champ.nombre LIKE '""" + busqueda + "%'")
+                rows = cursor.fetchall()
+
+    elif champ == "All":
         with connection.cursor() as cursor:
             cursor.execute("""SELECT champ.id, champ.nombre, roles.Rol, counter.counters FROM myapp_champ as champ
             JOIN (SELECT champrol.Champ_id, group_concat(roles.Rol, '@') as Rol FROM myapp_champrol as champrol
@@ -27,25 +66,7 @@ def obtenerChamps(champ):
                         GROUP BY counter.champ_selected_id) as counter ON counter.champ_selected_id = champ.id""")
             rows = cursor.fetchall()
 
-        listadoChamps = []#[champ1,champ2,champ3]
-        
-        for entry in rows:
-            champ = []
-            #champ.append(entry[0]) #id
-            champ.append(entry[1]) #nombre
-            rols = entry[2].split('@') # lista de roles
-            rols.pop()        
-            champ.append(rols)
-            counters = entry[3].split('@') # lista de counters
-            champ.append(counters)
-            listadoChamps.append(champ)
-
-        print ("="*100)
-        print (listadoChamps)
-        print ("="*100)
-
-        return listadoChamps
-    else:
+    elif champ != None:
         with connection.cursor() as cursor:
             cursor.execute("""  SELECT champ.id, champ.nombre, roles.Rol, counter.counters, counter.descrpipciones FROM myapp_champ as champ
 		                            JOIN (SELECT champrol.Champ_id, group_concat(roles.Rol, '@') as Rol FROM myapp_champrol as champrol
@@ -80,43 +101,57 @@ def obtenerChamps(champ):
             champ.append(countersCompletos)   
 
         return champ     
-    
+    if champ == 'Filtrar' or champ == 'All':
+        listadoChamps = []#[champ1,champ2,champ3]
+        
+        for entry in rows:
+            champ = []
+            #champ.append(entry[0]) #id
+            champ.append(entry[1]) #nombre
+            rols = entry[2].split('@') # lista de roles
+            rols.pop()        
+            champ.append(rols)
+            counters = entry[3].split('@') # lista de counters
+            champ.append(counters)
+            listadoChamps.append(champ)
+
+        print ("="*100)
+        print (listadoChamps)
+        print ("="*100)
+
+        return listadoChamps
 
 
 def index(request):
+    try:
+        nombreChamp = request.GET['Nombre']
+    except:
+        nombreChamp = None
+
+    try:
+        rol = request.GET['rol']
+    except:
+        rol = None
     
-    listadoChamps = obtenerChamps('All')
+    if rol:
+        if nombreChamp:
+            listadoChamps = obtenerChamps('Filtrar', rol, nombreChamp)
+        else:
+            listadoChamps = obtenerChamps('Filtrar', rol)
+    elif nombreChamp:
+        listadoChamps = obtenerChamps('Filtrar', None, nombreChamp)
+    else:
+        listadoChamps = obtenerChamps('All')
     
     return render (request,"index.html",
     {
        "listaChamps":(listadoChamps),
-       "buscar":buscar()
+       "buscar":buscar(),
+       "rol": request.GET.get("rol", ""),
+       "Nombre": request.GET.get("Nombre", "")
 
     }
     )
-
-
-
-def filter(request,lane):
-
-    #[11:14] Los filtros 
-    rolChamp = list(Roles.objects.filter(Rol = lane).values_list("id"))
-    try:
-        nombreChamp = request.GET['Nombre']
-    except:
-        nombreChamp = ""
-    #filtro_nombre = (request.GET["busqueda"])
-    champsPorRol = Champrol.objects.filter(Rol_id = rolChamp[0][0]).values_list("Champ_id")
-    champsPorRolYNombre = list(Champ.objects.filter(nombre__startswith = nombreChamp, id__in = champsPorRol).values_list("nombre"))
-    listaNombreChamps = []
-    for champ in champsPorRolYNombre:
-        listaNombreChamps.append(champ[0])
-    print ("=" * 100)
-    print (listaNombreChamps)
-    print ("=" * 100)
-
-
-    return render (request,"index.html",{"champs":(listaNombreChamps),"buscar":buscar()})
 
 def champ(request, champname):
 
